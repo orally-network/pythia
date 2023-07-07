a81758df4e9e2b59b1a5d4f4e95394d94ece46d8
Verkhohliad <ihor.verkhohliad@gmail.com>
2023-07-07 18:58:37 +0100

    add new multicall address 2

--- src/pythia/src/utils/multicall.rs
+++ src/pythia/src/utils/multicall.rs
@@ -21,7 +21,7 @@ use crate::{
 };
 
 const MULTICALL_ABI: &[u8] = include_bytes!("../../assets/MulticallABI.json");
-const MULTICALL_CONTRACT_ADDRESS: &str = "0x51324081c6483E6170379289a0A3CCC161835b39";
+const MULTICALL_CONTRACT_ADDRESS: &str = "0x88e33D0d7f9d130c85687FC73655457204E29467";
 const MULTICALL_CALL_FUNCTION: &str = "multicall";
 const MULTICALL_TRANSFER_FUNCTION: &str = "multitransfer";
 const BASE_GAS: u64 = 27_000;
